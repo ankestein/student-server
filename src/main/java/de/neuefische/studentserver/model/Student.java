598c96fbd35c38f834dc97547fdbc5b0f433f0d0
Anke <a.stein@posteo.de>
2021-09-23 17:14:32 +0200

    Add lombok dependency to pom.xml. Replace getters, setters, equals, hash code, and constructor with lombok annotation in Student

--- src/main/java/de/neuefische/studentserver/model/Student.java
+++ src/main/java/de/neuefische/studentserver/model/Student.java
@@ -1,42 +1,17 @@
 package de.neuefische.studentserver.model;
 
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.util.Objects;
 
+@Data
+@AllArgsConstructor
 public class Student {
 
     private int id;
     private String name;
 
-    public Student(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return id == student.id && Objects.equals(name, student.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
 }
